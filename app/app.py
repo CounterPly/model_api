6b426dde4e0a47d4610833d28ace74a2e9458e79
counterply <counterply@gmail.com>
2020-10-16 18:54:11 -0400

    Documentation and API Parameter rework

--- app/app.py
+++ app/app.py
@@ -21,8 +21,9 @@ def create_swagger(app_):
         "openapi": '3.0.0',
         "info": {
             "title": "Swagger API - Bentley Assignment",
+            "version": "1.0",
             "description": "OpenAPI 3.0 Specification for an Amazon EC2 Hosted ML Model",
-            "version": "1.0"
+
         },
         "basePath": "/",
         "schemes": [
@@ -41,7 +42,8 @@ model = pickle.load(pickle_in)
 
 @app.route('/')
 def welcome():
-    return "Welcome All"
+    from flask import request
+    return f"Please refer to the <a href=\"{request.url_root}apidocs\">API documentation</a> for more details."
 
 
 @app.route('/predict', methods=["GET"])
@@ -52,30 +54,66 @@ def predict():
     tags:
       - name: Single Prediction
     parameters:
-      - name: variance
+      - name: building
+        in: query
+        type: string
+        required: true
+        description: Name of a Building on the Main NIH Campus that is the location of work for an employee.
+      - name: num_publications
         in: query
         type: number
         required: true
-      - name: skewness
+        description: The total amount of peer-reviewed publications held by an employee.
+      - name: num_conferences
         in: query
         type: number
         required: true
-      - name: curtosis
+        description: The total amount of NIH-sanctioned conferences attended by an employee.
+      - name: status
+        in: query
+        type: string
+        required: true
+        description: The classification of an employee as either "Parttime" or "Fulltime" (40 hours/week).
+      - name: title
+        in: query
+        type: string
+        required: true
+        description: The official salutation of an employee; may correspond to educational level.
+      - name: expertise
+        in: query
+        type: string
+        required: true
+        description: The specific field of expertise held by an employee.
+      - name: institute
+        in: query
+        type: string
+        required: true
+        description: One of the 27 Institutes of the National Institutes of Health where the employee works.
+      - name: num_postdocs
         in: query
         type: number
         required: true
-      - name: entropy
+        description: The total amount of Postdoctoral Fellows that an employee oversees.
+      - name: num_reports
         in: query
         type: number
         required: true
+        description: The total amount of individuals (contractors and Federal workers) overseen by the employee.
     responses:
         200:
-            description: The output values
+            description: The output values of the request
     """
-    variance = request.args.get("variance")
-    skewness = request.args.get("skewness")
-    curtosis = request.args.get("curtosis")
-    entropy = request.args.get("entropy")
+    # All Parameters
+    building = request.args.get("building")
+    num_publications = request.args.get("num_publications")
+    num_conferences = request.args.get("num_conferences")
+    status = request.args.get("status")
+    title = request.args.get("title")
+    expertise = request.args.get("expertise")
+    institute = request.args.get("institute")
+    num_postdocs = request.args.get("num_postdocs")
+    num_reports = request.args.get("num_reports")
+
     prediction = model.predict([[variance, skewness, curtosis, entropy]])
     return str(prediction)
 
@@ -99,9 +137,8 @@ def predict_file(test_file):
     df_test = pd.read_csv(request.files.get(test_file))
     print(df_test.head())
     prediction = model.predict(df_test)
-
     return str(list(prediction))
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000)
+    app.run(host='0.0.0.0', port=5000)
