acb53353a04e4fd6047a96dc59fcb792405174c1
counterply <counterply@gmail.com>
2020-10-16 18:52:23 -0400

    Modify the landing page

--- model_engine.py
+++ model_engine.py
@@ -4,47 +4,6 @@
 import pickle
 import numpy as np
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier
-
-# Import from model_api/app/app.py.
-# from app.features import FEATURES
-
-# data = datasets.load_boston()
-# df = pd.DataFrame(data.data, columns=data.feature_names)
-# df['target'] = data.target
-
-
-def model_pipeline():
-    df = pd.read_csv('BankNote_Authentication.csv')
-
-    """Get the data, train the model, and save it."""
-    X, y = df.iloc[:, :-1], df.iloc[:, -1]
-    model = RandomForestClassifier(n_estimators=700)
-    model.fit(X, y)
-    pickle.dump(model, open('app/model.pkl', 'wb'))
-    print('Success!')
-
-
-def get_model_data(feature_names):
-    """Load and prepare data for modeling."""
-    df = build_dataframe()
-    df = clean_data(df)
-
-    # Limit the feature set for simplicity.
-    X, y = df[feature_names].values, df['target'].values
-    return X, y
-
-
-def build_dataframe():
-    """Build dataframe to facilitate cleaning."""
-    df = pd.read_csv('BankNote_Authentication.csv')
-    return df
-
-
-def clean_data(df):
-    """Clean data in preparation for modeling."""
-    return df
-
 
 if __name__ == '__main__':
-    model_pipeline()
+    joblib.load('model.joblib')
